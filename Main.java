dee6c5d7f95548a5cbe9b64108e5d6a5e179f78a
11sloe <43333134+11sloe@users.noreply.github.com>
2021-07-09 09:03:37 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,7 +7,7 @@ public class Main
         
         //String senseBoxId = "5d8de1a95f3de0001a86f3fb"; //Karlsfeld
         //String senseBoxId = "5c23af9c919bf8001a38c30d";   // Moosach
-        String senseBoxId = "606dabb74393eb001ca6a781";   // ITG
+        String senseBoxId = "60df298616d878001ba3116f";   // ITG
         //String senseBoxId = "sim"; // Simulation
         Messstation station = new Messstation(senseBoxId);
         
